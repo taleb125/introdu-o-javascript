0df37144fd5d45414e7abea6335be64ea8564b18
taleb125 <168199968+taleb125@users.noreply.github.com>
2025-11-10 10:30:04 -0300

    validoções de peso e altura

--- js/principal.js
+++ js/principal.js
@@ -7,13 +7,32 @@ var tdPeso = document.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
-var imc = peso / (altura*altura);
+var tdImc = paciente.querySelector(".info-imc");
 
+var pesoEhValido = true;
+var alturaEhValida = true;
+//validar os dados de peso
+if (peso <= 0 ||  peso >= 1000) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+}
+//validar a altura
+if (altura <=0 || altura > 3){
+    console.log("Altura inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+//validar peso e altura
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura*altura);
+    tdImc.textContent = imc;
+}
+
+/*
 console.log(paciente);  //tr
 console.log(tdPeso);    // td que tem peso
 console.log(tdAltura);  //td que tem altura
 console.log(peso);      //valor do peso
 console.log(altura);    //valor da altura
-
-var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
+*/
